8de1fe2d47f639de472c19433a71f918acdf838c
Mustafa Quraish <mustafaq9@gmail.com>
2024-02-01 00:41:36 -0500

    add missing doc libs

--- meta/gen_docs.sh
+++ meta/gen_docs.sh
@@ -14,6 +14,7 @@ import std::{ vector, map, compact_map, heap, deque, set, linkedlist }
 import std::{ buffer, bufferio, image, json, math, complex, fft, random }
 import std::{ value, vec, glut, socket, sort, thread, video_renderer, sv }
 import std::traits::{ hash, eq, compare }
+import std::{ image::draw, hash::sha1 }
 
 import std::sdl::{ ttf, image as sdl_img, mixer as sdl_mixer, gfx as sdl_gfx }
 
