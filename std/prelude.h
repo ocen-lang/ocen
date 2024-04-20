fb5e6feb15c593e01b38610618dc7f42d8785a3e
Mustafa Quraish <mustafaq9@gmail.com>
2024-04-20 15:55:49 -0400

    move format string function to ocen

--- std/prelude.h
+++ std/prelude.h
@@ -20,18 +20,6 @@ typedef uint64_t u64;
 typedef float f32;
 typedef double f64;
 
-char* format_string(const char* format, ...) {
-  va_list args;
-  va_start(args, format);
-  int size = vsnprintf(NULL, 0, format, args);
-  va_end(args);
-  va_start(args, format);
-  char* s = (char*)calloc(1, size + 1);
-  vsprintf(s, format, args);
-  s[size] = '\0';
-  va_end(args);
-  return s;
-}
 
 void ae_assert(int cond, char *dbg_msg, char *msg) {
   if (!cond) {
